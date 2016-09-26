fe1b7cfa6652ddaf02e0a450b14ecf3519e91e5e
Depechie <glenn.versweyveld@gmail.com>
2016-09-26 21:23:47 +0200

    Initial commit

---
+++ VisualStateTriggerVisibility/VisualStateTriggerVisibility/Converters/VisibilityConverter.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Reflection;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace VisualStateTriggerVisibility.Converters
+{
+    public class VisibilityConverter : IValueConverter
+    {
+        public VisibilityConverter()
+        {
+            SupportIsNullOrEmpty = true;
+            SupportCollectionEmpty = true;
+        }
+
+        /// <summary>
+        /// Invert the Visibility result value
+        /// </summary>
+        public bool Inverse { get; set; }
+
+        /// <summary>
+        /// Check for IsNullOrEmpty on string input values.
+        /// Returns Collapsed on an empty string
+        /// 
+        /// Default: true
+        /// </summary>
+        public bool SupportIsNullOrEmpty { get; set; }
+
+        /// <summary>
+        /// Check for an empty collection on input values.
+        /// Returns Collapsed if no elements are found.
+        /// 
+        /// Default: true
+        /// </summary>
+        public bool SupportCollectionEmpty { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            bool visible;
+
+            if (value is string && SupportIsNullOrEmpty)
+            {
+                visible = !string.IsNullOrEmpty(value.ToString());
+            }
+            else if (value is ICollection && SupportCollectionEmpty)
+            {
+                visible = ((ICollection)value).Count > 0;
+            }
+            else
+            {
+                var defaultValue = value != null ? GetDefaultValue(value.GetType()) : null;
+                visible = !Equals(value, defaultValue);
+            }
+
+            if (Inverse)
+                visible = !visible;
+
+            return visible ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotSupportedException();
+        }
+
+        private static object GetDefaultValue(Type type)
+        {
+            return type.GetTypeInfo().IsValueType ? Activator.CreateInstance(type) : null;
+        }
+    }
+}
---
+++ VisualStateTriggerVisibility/VisualStateTriggerVisibility/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Windows.UI.Xaml.Controls;
+
+namespace VisualStateTriggerVisibility
+{
+    public class Person
+    {
+        public string Name { get; set; }
+    }
+
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
+    {
+        public List<Person> People { get; set; }
+
+        private Person _selectedPerson;
+        public Person SelectedPerson
+        {
+            get { return _selectedPerson; }
+            set
+            {
+                if(value == _selectedPerson)
+                    return;
+
+                _selectedPerson = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            People = new List<Person>() { new Person() { Name = "Glenn" }, new Person() { Name = "Bart" }, new Person() { Name = "Nico" } };
+            DataContext = this;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
